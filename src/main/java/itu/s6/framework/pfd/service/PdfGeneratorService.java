7a9ff7ecfb0fd169909dff183a74b26adbf6948d
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-04-12 05:39:16 +0300

    create the html poc template

--- src/main/java/itu/s6/framework/pfd/service/PdfGeneratorService.java
+++ src/main/java/itu/s6/framework/pfd/service/PdfGeneratorService.java
@@ -14,18 +14,9 @@ import itu.s6.framework.pfd.model.Reservation;
 public class PdfGeneratorService {
 
     public byte[] generatePdf(Reservation reservation) throws Exception {
-        // Document document = new Document();
+        
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        // PdfWriter.getInstance(document, outputStream);
-        // document.open();
         
-        // // Ajoutez le contenu du PDF ici
-        // document.add(new Paragraph("Réservation ID: " + reservation.getId()));
-        // document.add(new Paragraph("Nom du client: " + reservation.getNomClient()));
-        // document.add(new Paragraph("Date de réservation: " + reservation.getDateReservation()));
-        // document.add(new Paragraph("Détails: " + reservation.getDetails()));
-        
-        // document.close();
         return outputStream.toByteArray();
     }
 
